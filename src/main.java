7389e8bd287c21485f68bb75a78e39a857ed89d0
Pez12 <151869239+Pez12@users.noreply.github.com>
2025-12-17 13:09:26 +0000

    Added hello world code

--- src/main.java
+++ src/main.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
-
+//Define a main class which will execute the application
 public class main {
+    //Define a main method to be executed
+    public static void main (String[] args) {
+        //Hello word code for initial commit
+        System.out.println("Hello, World!");
+    }
 }
