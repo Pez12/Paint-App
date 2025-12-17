ae1c5a7ba74b3ed2eed6d771c0c09689434e1472
Pez12 <151869239+Pez12@users.noreply.github.com>
2025-12-17 15:28:38 +0000

    Renamed Main class to align with naming conventions

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
-//Define a main class which will execute the application
-public class main {
+//Define a Main class which will execute the application
+public class Main {
     //Define a main method to be executed
     public static void main (String[] args) {
         //Hello word code for initial commit
