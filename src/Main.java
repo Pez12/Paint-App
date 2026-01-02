11d2a3dcef9a899b690937715417fae0b6741558
Pez12 <151869239+Pez12@users.noreply.github.com>
2026-01-02 18:45:02 +0000

    Added a TestWindow Class to practice using some Java Swift Classes

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
+//Import Packages
+import gui.*;
+
 //Define a Main class which will execute the application
 public class Main {
     //Define a main method to be executed
     public static void main (String[] args) {
-        //Hello word code for initial commit
-        System.out.println("Hello, World!");
+        //Bring up a Hello World GUI Window
+        TestWindow window = new TestWindow();
     }
 }
