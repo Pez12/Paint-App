0dfc034636d443f009e475190afe8fd9150cbfc4
Pez12 <151869239+Pez12@users.noreply.github.com>
2026-01-05 18:41:55 +0000

    Changed Testing Code

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,18 @@
 //Import Packages
 import gui.*;
+import java.util.HashMap;
+import javax.swing.JLabel;
 
 //Define a Main class which will execute the application
 public class Main {
     //Define a main method to be executed
     public static void main (String[] args) {
-        //Bring up a Hello World GUI Window
-        TestWindow test = new TestWindow();
+        //Test to see what happens if a key is entered into a hashMap that is already in use
+        HashMap<Integer, String> map = new HashMap<>();
+
+        map.put(1, "1");
+        System.out.println(map.get(1));
+        map.put(1, "2");
+        System.out.println(map.get(1));
     }
 }
--- src/gui/TestWindow.java
+++ src/gui/TestWindow.java
@@ -8,8 +8,9 @@ import java.awt.event.*;
 
 //Define a class that displays a test window (used for prototyping the GUI and getting familiar with the swing and awt libraries)
 
-//Had help from this tutorial: https://www.geeksforgeeks.org/java/introduction-to-java-swing/
-//Learnt about Jbuttons from: https://docs.oracle.com/javase/tutorial/uiswing/components/button.html
+/*Had help from this tutorial: https://www.geeksforgeeks.org/java/introduction-to-java-swing/
+Learnt about Jbuttons from: https://docs.oracle.com/javase/tutorial/uiswing/components/button.html
+*/
 
 
 public class TestWindow extends Window implements ActionListener {
